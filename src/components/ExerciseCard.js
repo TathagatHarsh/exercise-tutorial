e2dad9e1591918e4f02da47f642dd80c42405a6f
Tathagat Harsh <73013331+TathagatHarsh@users.noreply.github.com>
2025-05-07 17:00:10 +0530

    Updated Exercise Card

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -1,47 +1,51 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button, Stack, Typography } from "@mui/material";
 
 const ExerciseCard = ({ exercise }) => (
   <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
     <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
-    <Stack direction="row">
-      <Button
-        sx={{
-          ml: "21px",
+    <div className="flex flex-row">
+      <button
+        style={{
+          marginLeft: "21px",
           color: "#fff",
-          background: "#FFA9A9",
+          backgroundColor: "#FFA9A9",
           fontSize: "14px",
           borderRadius: "20px",
           textTransform: "capitalize",
         }}
+        className="px-4 py-1"
       >
         {exercise.bodyPart}
-      </Button>
-      <Button
-        sx={{
-          ml: "21px",
+      </button>
+      <button
+        style={{
+          marginLeft: "21px",
           color: "#fff",
-          background: "#FCC757",
+          backgroundColor: "#FCC757",
           fontSize: "14px",
           borderRadius: "20px",
           textTransform: "capitalize",
         }}
+        className="px-4 py-1"
       >
         {exercise.target}
-      </Button>
-    </Stack>
-    <Typography
-      ml="21px"
-      color="#000"
-      fontWeight="bold"
-      sx={{ fontSize: { lg: "24px", xs: "20px" } }}
-      mt="11px"
-      pb="10px"
-      textTransform="capitalize"
+      </button>
+    </div>
+    <p
+      style={{
+        marginLeft: "21px",
+        color: "#000",
+        fontWeight: "bold",
+        marginTop: "11px",
+        paddingBottom: "10px",
+        textTransform: "capitalize",
+        fontSize: "24px",
+      }}
+      className="text-lg md:text-xl"
     >
       {exercise.name}
-    </Typography>
+    </p>
   </Link>
 );
 
