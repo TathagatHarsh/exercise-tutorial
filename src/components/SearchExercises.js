c21a00457ff615b395a0fe51679dfdd412cead12
Tathagat Harsh <73013331+TathagatHarsh@users.noreply.github.com>
2025-05-07 13:39:02 +0530

    Updated Search Exercise

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,14 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { Box, Button, Stack, TextField, Typography } from "@mui/material";
-
 import { exerciseOptions, fetchData } from "../utils/fetchData";
 import HorizontalScrollbar from "./HorizontalScrollbar";
 
 const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   const [search, setSearch] = useState("");
   const [bodyParts, setBodyParts] = useState([]);
-  console.log("Search query:", search);
-  console.log(bodyPart);
 
   useEffect(() => {
     const fetchExercisesData = async () => {
@@ -16,7 +12,6 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
         "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
         exerciseOptions
       );
-
       setBodyParts(["all", ...bodyPartsData]);
     };
 
@@ -46,55 +41,34 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   };
 
   return (
-    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
-      <Typography
-        fontWeight={700}
-        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
-        mb="49px"
-        textAlign="center"
-      >
-        Awesome Exercises You <br /> Should Know
-      </Typography>
-      <Box position="relative" mb="72px">
-        <TextField
-          height="76px"
-          sx={{
-            input: { fontWeight: "700", border: "none", borderRadius: "4px" },
-            width: { lg: "1170px", xs: "350px" },
-            backgroundColor: "#fff",
-            borderRadius: "40px",
-          }}
+    <div className="flex flex-col items-center px-5 py-6 mt-10">
+      <h2 className="font-extrabold text-center text-[30px] md:text-[44px] mb-12">
+        Awesome Exercises You <br className="hidden md:block" /> Should Know
+      </h2>
+      <div className="relative mb-20 w-full max-w-[1170px]">
+        <input
+          type="text"
+          placeholder="Search Exercises"
           value={search}
           onChange={(e) => setSearch(e.target.value.toLowerCase())}
-          placeholder="Search Exercises"
-          type="text"
+          className="w-full h-[76px] pl-6 pr-[180px] bg-white text-black font-bold rounded-full shadow-md focus:outline-none text-base md:text-lg"
         />
-        <Button
-          className="search-btn"
-          sx={{
-            bgcolor: "#007bff",
-            color: "#fff",
-            textTransform: "none",
-            width: { lg: "173px", xs: "80px" },
-            height: "56px",
-            position: "absolute",
-            right: "0px",
-            fontSize: { lg: "20px", xs: "14px" },
-          }}
+        <button
           onClick={handleSearch}
+          className="absolute right-2 top-1/2 -translate-y-1/2 bg-blue-600 text-white px-5 md:px-10 h-[56px] rounded-full text-sm md:text-lg hover:bg-blue-700 transition-colors"
         >
           Search
-        </Button>
-      </Box>
-      <Box sx={{ position: "relative", width: "100%", p: "20px" }}>
+        </button>
+      </div>
+      <div className="w-full px-5">
         <HorizontalScrollbar
           data={bodyParts}
           bodyParts
           setBodyPart={setBodyPart}
           bodyPart={bodyPart}
         />
-      </Box>
-    </Stack>
+      </div>
+    </div>
   );
 };
 
